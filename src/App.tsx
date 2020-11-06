405fa3920478a09aa5689e12c9220fd7831a0c08
june3141 <ryoushi0620@gmail.com>
2020-11-06 14:48:23 +0900

    title の変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Header } from './components/Header';
 import { ContentsTabs } from './components/ContentsTabs';
+import { Biography } from './components/Biography';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <Header />
+      <Biography />
       <ContentsTabs />
     </div>
   );
---
+++ src/components/Biography.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Biography: React.FC = () => {
+  return <h2>Biography Contents.</h2>;
+};
